0c7888bf19d8111ce5b9246542a969a11be46bec
Natália Fonseca <nataliabf@outlook.com>
2022-01-06 13:19:17 -0300

    mais testes para query string

--- modulo1/src/lib/queryString.spec.js
+++ modulo1/src/lib/queryString.spec.js
@@ -9,4 +9,28 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Fabio&profession=developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Fabio',
+      profession: 'developer',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe(
+      'name=Fabio&profession=developer&abilities=JS,TDD',
+    );
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Fabio',
+      profession: 'developer',
+      abilities: { first: 'JS', second: 'TDD' },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
