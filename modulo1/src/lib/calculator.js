845d573dad5244104a875655ba83762c44ae0aaa
Natália Fonseca <nataliabf@outlook.com>
2022-01-06 00:19:02 -0300

    configura prettier

--- modulo1/src/lib/calculator.js
+++ modulo1/src/lib/calculator.js
@@ -1,6 +1,6 @@
 module.exports.sum = (num1, num2) => {
-  const int1 = parseInt(num1, 10)
-  const int2 = parseInt(num2, 10)
+  const int1 = parseInt(num1, 10);
+  const int2 = parseInt(num2, 10);
 
   if (Number.isNaN(int1) || Number.isNaN(int2)) {
     throw new Error('Please check your input');
