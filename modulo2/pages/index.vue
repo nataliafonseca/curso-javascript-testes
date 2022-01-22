238a6b30cf265c400a309a32a508fe6736555c7a
Natália Fonseca <nataliabf@outlook.com>
2022-01-22 00:15:20 -0300

    mudança nas configurações do prettier

--- modulo2/pages/index.vue
+++ modulo2/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="my-8">
-    <div class="container mx-auto px-6">
-      <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
+    <div class="container px-6 mx-auto">
+      <h3 class="text-2xl font-medium text-gray-700">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
       <div
-        class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
+        class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
       >
         <product-card v-for="product in products" :key="product.id" />
       </div>
@@ -13,14 +13,14 @@
 </template>
 
 <script>
-import ProductCard from '@/components/ProductCard'
-import products from '@/mocks/products.json'
+import ProductCard from '@/components/ProductCard';
+import products from '@/mocks/products.json';
 export default {
   components: { ProductCard },
   computed: {
     products() {
-      return products
+      return products;
     },
   },
-}
+};
 </script>
