b402e281c59a8e7367e2cf4a4c9dc64c46bfc3c5
Natália Fonseca <nataliabf@outlook.com>
2022-02-14 09:14:58 -0300

    implementação da busca com tdd

--- modulo2/pages/index.vue
+++ modulo2/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="my-8">
-    <search />
+    <search @doSearch="setSearchTerm" />
     <div v-if="errorMessage === ''" class="container px-6 mx-auto">
       <h3 class="text-2xl font-medium text-gray-700">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
@@ -8,7 +8,7 @@
         class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
       >
         <product-card
-          v-for="product in products"
+          v-for="product in list"
           :key="product.id"
           :product="product"
         />
@@ -25,7 +25,17 @@ import Search from '@/components/Search';
 export default {
   components: { ProductCard, Search },
   data() {
-    return { products: [], errorMessage: '' };
+    return { products: [], errorMessage: '', searchTerm: '' };
+  },
+  computed: {
+    list() {
+      if (this.searchTerm !== '') {
+        return this.products.filter(({ title }) =>
+          title.includes(this.searchTerm)
+        );
+      }
+      return this.products;
+    },
   },
   async created() {
     try {
@@ -33,6 +43,11 @@ export default {
     } catch (error) {
       this.errorMessage = 'Problema ao carregar a lista de produtos.';
     }
+  },
+  methods: {
+    setSearchTerm({ term }) {
+      this.searchTerm = term;
+    },
   },
 };
 </script>
