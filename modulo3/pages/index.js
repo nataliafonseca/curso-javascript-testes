9ffcdafd8c2c0c17a30ef421c87fccb41817f422
Natália Fonseca <nataliabf@outlook.com>
2022-02-17 09:31:33 -0300

    separação entre unit e integration tests

--- modulo3/pages/index.js
+++ modulo3/pages/index.js
@@ -1,14 +1,14 @@
-import ProductCard from '../components/product-card';
-import Search from '../components/search';
+import ProductCard from "../components/product-card";
+import Search from "../components/search";
 
 export default function Home() {
   return (
-    <main className="my-8">
+    <main data-testid="home" className="my-8">
       <Search />
-      <div className="container mx-auto px-6">
-        <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
+      <div className="container px-6 mx-auto">
+        <h3 className="text-2xl font-medium text-gray-700">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+        <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           <ProductCard />
         </div>
       </div>
