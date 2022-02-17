31723fb85daefca5f37a72f38ab85ee52c1d49f9
Natália Fonseca <nataliabf@outlook.com>
2022-02-17 08:17:43 -0300

    testes do cart-item

--- modulo3/components/cart-item.js
+++ modulo3/components/cart-item.js
@@ -1,18 +1,29 @@
-export default function CartItem() {
+import { useState } from "react";
+
+export default function CartItem({ product }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const increase = () => setQuantity(quantity + 1);
+  const decrease = () => setQuantity(quantity > 0 ? quantity - 1 : 0);
+
   return (
-    <div className="flex justify-between mt-6">
+    <div data-testid="cart-item" className="flex justify-between mt-6">
       <div className="flex">
         <img
-          className="h-20 w-20 object-cover rounded"
-          src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-          alt=""
+          data-testid="image"
+          className="object-cover w-20 h-20 rounded"
+          src={product.image}
+          alt={product.title}
         />
         <div className="mx-3">
-          <h3 className="text-sm text-gray-600">Mac Book Pro</h3>
+          <h3 className="text-sm text-gray-600">{product.title}</h3>
           <div className="flex items-center mt-2">
-            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+            <button
+              className="text-gray-500 focus:outline-none focus:text-gray-600"
+              onClick={decrease}
+            >
               <svg
-                className="h-5 w-5"
+                className="w-5 h-5"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -23,10 +34,15 @@ export default function CartItem() {
                 <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
-            <span className="text-gray-700 mx-2">2</span>
-            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+            <span data-testid="quantity" className="mx-2 text-gray-700">
+              {quantity}
+            </span>
+            <button
+              className="text-gray-500 focus:outline-none focus:text-gray-600"
+              onClick={increase}
+            >
               <svg
-                className="h-5 w-5"
+                className="w-5 h-5"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -40,7 +56,7 @@ export default function CartItem() {
           </div>
         </div>
       </div>
-      <span className="text-gray-600">20$</span>
+      <span className="text-gray-600">${product.price}</span>
     </div>
   );
 }
