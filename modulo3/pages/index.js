3a3bfe219d2c7114ac5d0452b454a3ee8937f8dd
Natália Fonseca <nataliabf@outlook.com>
2022-02-26 22:43:52 -0300

    inicio dos testes de integração

--- modulo3/pages/index.js
+++ modulo3/pages/index.js
@@ -1,15 +1,23 @@
 import ProductCard from "../components/product-card";
 import Search from "../components/search";
 
+import { useFetchProducts } from "../hooks/use-fetch-products";
+
 export default function Home() {
+  const { products, error } = useFetchProducts();
+
   return (
-    <main data-testid="home" className="my-8">
+    <main data-testid="product-list" className="my-8">
       <Search />
       <div className="container px-6 mx-auto">
         <h3 className="text-2xl font-medium text-gray-700">Wrist Watch</h3>
-        <span className="mt-3 text-sm text-gray-500">200+ Products</span>
+        <span className="mt-3 text-sm text-gray-500">
+          {products.length} Products
+        </span>
         <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <ProductCard />
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
         </div>
       </div>
     </main>
