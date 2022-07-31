6d5d993089a00ce6ecd4bc8c37a8c95895192847
Natália Fonseca <nataliabf@outlook.com>
2022-07-31 17:57:12 -0300

    configuração do ambiente de testes

---
+++ modulo3/components/search.spec.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import Search from './search';
+
+describe('Search', () => {
+  it('should render Search component', () => {
+    render(<Search />);
+
+    expect(screen.getByTestId('search')).toBeInTheDocument();
+  });
+});
